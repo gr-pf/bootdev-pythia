0a9987820907abff122658e94fb6d212316601b1
l4sd4 <237513179+gr-pf@users.noreply.github.com>
2026-04-20 16:25:37 +0200

    Update tests

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -5,4 +5,4 @@ def test_merge_sort(input_expected):
     input_list, expected_list = input_expected
     result_list = merge_sort(input_list)
     assert result_list == expected_list
-    # assert input_list is not result_list
+    assert input_list is not result_list
