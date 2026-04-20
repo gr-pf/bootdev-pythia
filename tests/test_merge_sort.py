346c85b09da5a44791f8b270bf836543fdcb9ac8
l4sd4 <237513179+gr-pf@users.noreply.github.com>
2026-04-20 16:21:04 +0200

    Add merge sort tests

---
+++ tests/test_merge_sort.py
@@ -0,0 +1,8 @@
+from model.algos.merge_sort import merge_sort
+
+
+def test_merge_sort(input_expected):
+    input_list, expected_list = input_expected
+    result_list = merge_sort(input_list)
+    assert result_list == expected_list
+    # assert input_list is not result_list
