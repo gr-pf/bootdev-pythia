a8e7cd16d3086f4a734428c5875ecabb2aeb2eee
l4sd4 <237513179+gr-pf@users.noreply.github.com>
2026-04-20 15:49:49 +0200

    Update bubble sort with optimised and non optimised versions

--- model/algos/bubble_sort.py
+++ model/algos/bubble_sort.py
@@ -1,17 +1,31 @@
 # Algo bubble sort (on fixe le max à chaque passage)
+# Raw and optimised (si on ne modifie pas le tableau lors d'une boucle c'est qu'il est déjà trié)
 
 
-def bubble_sort(input_list: list):
+def bubble_sort_raw(input_list: list):
     copy_list = input_list.copy()
-    swapping = True
-    end = len(copy_list)
 
-    while swapping:
-        swapping = False
-        for i in range(1, end):
-            if copy_list[i - 1] > copy_list[i]:
-                copy_list[i - 1], copy_list[i] = copy_list[i], copy_list[i - 1]
-                swapping = True
-        end -= 1
+    for i in range(len(copy_list) - 1, 0, -1):
+        for j in range(0, i):
+            if copy_list[j + 1] < copy_list[j]:
+                copy_list[j + 1], copy_list[j] = copy_list[j], copy_list[j + 1]
+
+    return copy_list
+
+
+print(bubble_sort_raw([4, 3, 2, 1, 0]))
+
+
+def bubble_sort_optimised(input_list: list):
+    copy_list = input_list.copy()
+
+    for i in range(len(copy_list) - 1, 0, -1):
+        list_sorted = True
+        for j in range(0, i):
+            if copy_list[j + 1] < copy_list[j]:
+                copy_list[j + 1], copy_list[j] = copy_list[j], copy_list[j + 1]
+                list_sorted = False
+        if list_sorted:
+            return copy_list
 
     return copy_list
--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -1,8 +1,15 @@
-from model.algos.bubble_sort import bubble_sort
+from model.algos.bubble_sort import bubble_sort_raw, bubble_sort_optimised
 
 
-def test_bubble_sort(input_expected):
+def test_bubble_sort_raw(input_expected):
     input_list, expected_list = input_expected
-    result_list = bubble_sort(input_list)
+    result_list = bubble_sort_raw(input_list)
+    assert result_list == expected_list
+    assert input_list is not result_list
+
+
+def test_bubble_sort_optimised(input_expected):
+    input_list, expected_list = input_expected
+    result_list = bubble_sort_optimised(input_list)
     assert result_list == expected_list
     assert input_list is not result_list
