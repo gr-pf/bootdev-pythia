2580c092f023a6e3745af3c35db0e2be0b7a96fa
last day <237513179+gr-pf@users.noreply.github.com>
2026-06-11 18:29:01 +0200

    Add page de tri

---
+++ view/page01_tri.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+
+from rich.console import Console
+from rich.panel import Panel
+from rich import box
+
+
+def page_tri():
+    os.system("cls||clear")
+
+    console = Console()
+
+    console.print("Page Tri - A venir")
+
+
+if __name__ == "__main__":
+    page_tri()
