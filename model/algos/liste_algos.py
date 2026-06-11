d6b2bd879ec4b3ce1155bd0d09b3ad4170baac5b
last day <237513179+gr-pf@users.noreply.github.com>
2026-06-11 19:25:13 +0200

    Création d'une liste d'algo pour affichage

---
+++ model/algos/liste_algos.py
@@ -0,0 +1,34 @@
+list_aglos = [
+    [
+        "1",
+        "Buildin sort",
+    ],
+    [
+        "2",
+        "Buildin sorted",
+    ],
+    [
+        "3",
+        "Bubble sort",
+    ],
+    [
+        "4",
+        "Bubble sort optimisée",
+    ],
+    [
+        "5",
+        "Insertion sort",
+    ],
+    [
+        "6",
+        "Merge sort",
+    ],
+    [
+        "7",
+        "Selection sort",
+    ],
+    [
+        "8",
+        "Quick sort",
+    ],
+]
