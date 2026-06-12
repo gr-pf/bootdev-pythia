8469fd8de3a3f2db591cf10412be027234e89e36
last day <237513179+gr-pf@users.noreply.github.com>
2026-06-12 10:54:53 +0200

    Update liste_algo

--- model/algos/liste_algos.py
+++ model/algos/liste_algos.py
@@ -1,34 +1,37 @@
+from collections import namedtuple
+
+AlgoType = namedtuple("AlgoType", ["id", "name"])
 list_aglos = [
-    [
+    AlgoType(
         "1",
         "Buildin sort",
-    ],
-    [
+    ),
+    AlgoType(
         "2",
         "Buildin sorted",
-    ],
-    [
+    ),
+    AlgoType(
         "3",
         "Bubble sort",
-    ],
-    [
+    ),
+    AlgoType(
         "4",
         "Bubble sort optimisée",
-    ],
-    [
+    ),
+    AlgoType(
         "5",
         "Insertion sort",
-    ],
-    [
+    ),
+    AlgoType(
         "6",
         "Merge sort",
-    ],
-    [
+    ),
+    AlgoType(
         "7",
         "Selection sort",
-    ],
-    [
+    ),
+    AlgoType(
         "8",
         "Quick sort",
-    ],
+    ),
 ]
