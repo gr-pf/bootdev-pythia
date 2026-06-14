214ac18fe35925846aaf1806f8f77b6d3b161fe9
last day <237513179+gr-pf@users.noreply.github.com>
2026-06-14 14:40:23 +0200

    Update var names

--- view/page00_home.py
+++ view/page00_home.py
@@ -13,35 +13,35 @@ def page_home() -> PageResult:
     console = Console()
     console.rule("ellipsis")
 
-    home_main_panel = """
+    text_main_panel = """
     PYTHIA
     Oracle de vos algos
 
     v. 0.1.0
 """
 
-    home_main_content = """
+    text_main_content = """
 Pythia est un outil CLI en Python conçu pour révéler ce qui se cache derrière les performances des algorithmes et structures de données. Il vise à comparer différentes implémentation — librairie standard, pur Python ou librairie dédiée à l'optimisation. Son objectif est d’apprendre l’algorithmie de manière concrète, expérimentale et mesurable.
 
 Il est développé dans le cadre du programme d'apprentissage du code de boot.dev
 """
 
-    home_choices = """
+    text_choices = """
 Q. Quitter
 1. Algo de Tri
 2. A venir
 """
 
-    home_input = """
+    text_input = """
 Quel est votre choix ?
 >>> """
 
-    console.print(Panel(home_main_panel, style="color(12)", box=box.DOUBLE_EDGE))
-    console.print(home_main_content)
-    console.print(home_choices)
+    console.print(Panel(text_main_panel, style="color(12)", box=box.DOUBLE_EDGE))
+    console.print(text_main_content)
+    console.print(text_choices)
 
     while True:
-        user_input = console.input(home_input)
+        user_input = console.input(text_input)
         match user_input.lower():
             case "q" | "1" | "2":
                 return PageResult("home", user_input.lower())
